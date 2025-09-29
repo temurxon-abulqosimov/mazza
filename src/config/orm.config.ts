cc1fa9055a5e510211acdc6c417790916de40052
Temurxs <abulqosimovtemur86@gmail.com>
2025-09-29 15:55:26 +0500

    Simplify database config to use single DATABASE_URL

--- src/config/orm.config.ts
+++ src/config/orm.config.ts
@@ -1,14 +1,9 @@
 import { DataSourceOptions } from "typeorm";
 import { envVariables } from "./env.variables";
 
-
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: envVariables.DB_HOST,
-  port: envVariables.DB_PORT,
-  username: envVariables.DB_USERNAME,
-  password: envVariables.DB_PASSWORD,
-  database: envVariables.DB_NAME,
+  url: envVariables.DATABASE_URL,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   migrations: ['dist/migrations/*.js'],
   synchronize: true,
